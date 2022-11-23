6e512f9f4714ab85ae6f0fdb3c281e0a0d51d941
ArmandoMartiny <71964707+ArmandoMartiny@users.noreply.github.com>
2022-11-23 00:12:56 -0600

    first commit

---
+++ Introduccion_al_backend/BUENASPRACTICASJAVASCRIPT/index.js
@@ -0,0 +1,40 @@
+document.getElementById("parrafoInicial").onclick = function () {
+    accion();
+}
+
+function accion() {
+    alert("Holiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+}
+
+console.log("Hola desde la terminal")
+/* 
+tipos de variables
+var -> Modificarse, scope global - no se recomienda
+
+let -> Modificarse, scope limitado - RESCOMENDADA DE USAR
+
+const -> CONSTANTE, no modificable - Scope global
+ */
+/* CASES
+
+PascalCase -> Clases -> JAVA
+
+camelCase -> variables - funciones - atributos - Java, JavaScript etc.
+    variable -> parrafoInicial =
+    funcion -> parrafoInicial()
+    atributo -> parrafoInicial
+
+snake_case -> Python
+        variables & funciones
+
+CASE -> Constantes  
+
+ */
+
+/* console.log(parrafoInicial)
+parrafoInicial = 6
+console.log(parrafoInicial)
+ */
+/* parrafoInicial.onclick = function () {
+    accion()
+} */
---
+++ Introduccion_al_backend/curiosidades.md
@@ -0,0 +1,2 @@
+1.- ECMA Script = Standard de javascript -> Actualmente la version mas reciente es la 6
+2.- ECMA = European Computer Manufacturers Association
